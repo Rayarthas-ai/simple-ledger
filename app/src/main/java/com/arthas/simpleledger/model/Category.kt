0d1d2801a611579a50e45b7a4963f64af66e9902
Raymand <zhao706916@gmail.com>
2026-08-13 14:15:42 +0800

    Add domain models

---
+++ app/src/main/java/com/arthas/simpleledger/model/Category.kt
@@ -0,0 +1,15 @@
+package com.arthas.simpleledger.model
+
+enum class Category(val label: String) {
+    FOOD("餐饮"),
+    SNACK("零食"),
+    RENT("房租"),
+    WIFI("WiFi"),
+    ELECTRICITY("电费"),
+    WATER("水费"),
+    TRANSPORT("交通"),
+    DAILY("日用品"),
+    MEDICAL("医疗"),
+    GAME("游戏"),
+    OTHER("其他")
+}
